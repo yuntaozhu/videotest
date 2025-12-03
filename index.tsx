e4309ba99cc2413d49716cfd32bd0d8ac9e0634d
immanuel <immanuel.zhu@live.com>
2025-12-03 10:14:51 +0800

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
